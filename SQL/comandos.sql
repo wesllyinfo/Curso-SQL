31244fba8bcc1804c8aa52fbfbbd01fd26587c38
Weslly Silva <wesllyinfo@gmail.com>
2023-01-17 08:34:20 -0300

    Função Having

--- SQL/comandos.sql
+++ SQL/comandos.sql
@@ -311,6 +311,35 @@ group by state
 -- (ex: GROUP BY 1,2,3 irá agrupar pelas 3 primeiras colunas da tabela) 
 -- (4) O GROUP BY sozinho funciona como um DISTINCT, eliminando linhas duplicadas
 
+-- PARA QUE SERVE ##################################################################
+-- Serve para filtrar linhas da seleção por uma coluna agrupada
+
+
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) seleção com filtro no HAVING 
+-- Calcule o nº de clientes por estado filtrando apenas estados acima de 100 clientes
+select 
+    state, 
+    count(*)
+from sales.customers
+group by state
+having count(*) > 100
+    and state <> 'MG'
+
+
+-- RESUMO ##########################################################################
+-- (1) Tem a mesma função do WHERE mas pode ser usado para filtrar os resultados 
+-- das funções agregadas enquanto o WHERE possui essa limitação
+-- (2) A função HAVING também pode filtrar colunas não agregadas
+
+
+
+
+
+
+
+
 
 
 
