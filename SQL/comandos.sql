1c9c679d62c74caaae37965dc079605c941c80ae
Weslly Silva <wesllyinfo@gmail.com>
2023-02-02 08:28:02 -0300

    Comando Union

--- SQL/comandos.sql
+++ SQL/comandos.sql
@@ -435,3 +435,28 @@ left join temp_tables.regions as reg
 group by reg.region
 order by visitas desc
 
+
+-- SINTAXE #########################################################################
+select coluna_1, coluna_2
+from schema_1.tabela_1
+
+union / union all
+
+select coluna_3, coluna_4 
+from schema_2.tabela_2
+
+
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) União simples de duas tabelas
+-- Una a tabela sales.products com a tabela temp_tables.products_2
+
+select * from sales.products
+
+select * from temp_tables.products_2
+
+
+
+
+
+
